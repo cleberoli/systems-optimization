24b329ac2485de7f09e59246c5028287dc1a252e
Cleber Oliveira <cleber-oliveira@outlook.com>
2017-05-11 16:44:34 -0300

    Section 5.1 Started 5.1 - 10 (wrong answer) 5.2 - 1 (wrong answer)

---
+++ transportation1023.sol
@@ -0,0 +1,53 @@
+c Problem:    Transportation 10 - College
+c Rows:       16
+c Columns:    28
+c Non-zeros:  112
+c Status:     INFEASIBLE (FINAL)
+c Objective:  z = 90 (MINimum)
+c
+s bas 16 28 n i 90
+i 1 b -1 0
+i 2 l -2 0
+i 3 l -2 0
+i 4 b -1.36842105263158 0
+i 5 l 1 0
+i 6 l 1 0
+i 7 l 1 0
+i 8 l 1 0
+i 9 l 1 0
+i 10 l 1 0
+i 11 b 0.368421052631579 0
+i 12 b 10 0
+i 13 b 31 0
+i 14 b 27 0
+i 15 b 22 0
+i 16 u 90 1
+j 1 b 1 0
+j 2 l 0 0
+j 3 l 0 0
+j 4 l 0 0
+j 5 l 0 0
+j 6 l 0 0
+j 7 l 0 0
+j 8 l 0 0
+j 9 b 1 0
+j 10 b 0 0
+j 11 b 1 0
+j 12 l 0 0
+j 13 l 0 0
+j 14 l 0 0
+j 15 l 0 0
+j 16 l 0 0
+j 17 l 0 0
+j 18 b 0 0
+j 19 b 1 0
+j 20 b 1 0
+j 21 l 0 0
+j 22 l 0 0
+j 23 l 0 0
+j 24 b 1 0
+j 25 l 0 0
+j 26 l 0 0
+j 27 l 0 0
+j 28 b 0.368421052631579 0
+e o f
